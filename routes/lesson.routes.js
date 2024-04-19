95ca1e7fef8e532353bab1ff133facc1d3164217
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-04-19 23:23:54 +0200

    add edit route to lessons

--- routes/lesson.routes.js
+++ routes/lesson.routes.js
@@ -35,13 +35,90 @@ router.post('/courses/:courseId/lessons/create', async (req, res, next) => {
 
     res.status(200).json({message: "Lesson created and added to course", createdLesson});
 
+  }catch(error){
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
+
+// get All lessons for a course for an ongoing lesson;
+router.get('/learn/courses/:courseId/lessons', (req, res, next) => {
+  const { courseId } = req.params;
+
+  const { limit, offset } = req.query;
+
+  const limitValue = parseInt(limit) || 1;
+  const offsetValue = parseInt(offset) || 0;
+
+  Course.findOne({ _id: courseId })
+    .populate('lessons', '-__v')
+    .select('lessons')
+    .then(course => {
+      console.log(course)
+      if (!course) {
+        return res.status(404).json({ message: 'Course not found' });
+      }
+
+      const totalCount = course.lessons.length;
+
+      const totalPages = Math.ceil(totalCount / limitValue);
+
+      Course.findOne({ _id: courseId })
+        .populate({
+          path: 'lessons',
+          select: '-__v',
+          options: {
+            limit: limitValue,
+            skip: offsetValue
+          }
+        })
+        .select('lessons')
+        .then(paginatedCourse => {
+          res.status(200).json({ lessons: paginatedCourse.lessons, totalPages });
+        })
+        .catch(error => res.status(500).json({ message: 'Ooops, something went wrong!' }));
+    })
+    .catch(error => res.status(500).json({ message: 'Ooops, something went wrong!' }));
+});
+
+
+// edit a lesson
+router.put('/courses/:courseId/lessons/:lessonId', async (req, res, next) => {
+  try{
+    const { courseId, lessonId } = req.params;
+    const { name, description, content, estimatedDuration, level, programmingLanguage } = req.body;
+
+    const course = await Course.findOne({ _id: courseId }).populate('lessons');
+
+    if(!course){
+      res.status(500).json({ message: "Course not found!" });
+      return;
+    }
+
+    const lessonToUpdate = course.lessons.find(lesson => lesson._id.toString() === lessonId);
+
+    if(!lessonToUpdate){
+      res.status(500).json({ message: "Lesson not found!" });
+      return;
+    }
+
+    lessonToUpdate.name = name;
+    lessonToUpdate.description = description;
+    lessonToUpdate.content = content;
+    lessonToUpdate.estimatedDuration = estimatedDuration;
+    lessonToUpdate.level = level;
+    lessonToUpdate.programmingLanguage = programmingLanguage;
+
+    const updatedLesson = await lessonToUpdate.save();
+
+    res.status(200).json({ message: "Lesson updated successfully", updatedLesson: updatedLesson });
 
   }catch(error){
     console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
-})
+});
 
 
 module.exports = router;
